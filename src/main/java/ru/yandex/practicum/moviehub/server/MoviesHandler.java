39bcfc265c210a6ac1ab466313f656b841d25b56
Igor <igorprasolov00259@gmail.com>
2026-06-08 17:31:28 +0300

    #4 fix checkstyle: remove unused import

--- src/main/java/ru/yandex/practicum/moviehub/server/MoviesHandler.java
+++ src/main/java/ru/yandex/practicum/moviehub/server/MoviesHandler.java
@@ -12,7 +12,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 class MoviesHandler extends BaseHttpHandler {
     private final MoviesStore store;
