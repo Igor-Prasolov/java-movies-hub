c8328d146e04ccab408e6cf39e390bd0796239f6
Igor <igorprasolov00259@gmail.com>
2026-06-08 12:54:59 +0300

    fix: rename Id to id for checkstyle

--- src/test/java/ru/yandex/practicum/moviehub/test/MoviesApiTest.java
+++ src/test/java/ru/yandex/practicum/moviehub/test/MoviesApiTest.java
@@ -252,10 +252,10 @@ public class MoviesApiTest {
         JsonObject jsonObject = jsonElement.getAsJsonObject();
         assertEquals(201, response.statusCode());
 
-        int Id = jsonObject.get("id").getAsInt();
+        int id = jsonObject.get("id").getAsInt();
 
         HttpRequest getRequest = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies/" + Id))
+                .uri(URI.create(BASE + "/movies/" + id))
                 .GET()
                 .build();
 
